41caf5bde4a6f5d3b2d3c8e417eea600c833a83e
gg <chengang2022@gmail.com>
2018-04-01 11:54:44 +0800

    chart demo

--- option/views.py
+++ option/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+import logging
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('option/index.html')
+    context = {
+        'hello': 'world',
+    }
+    return HttpResponse(template.render(context, request))
